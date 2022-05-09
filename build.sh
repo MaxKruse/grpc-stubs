c05a38bc70ad4a0c2e5ac495706a75a91dd340d2
Maximilian Kruse <mkruse@exceet.de>
2022-05-09 14:16:59 +0200

    Add User file

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./authenticate/authenticate.proto
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./authenticate/Authenticate.proto
 
-protoc --proto_path=./authenticate --js_out=import_style=commonjs,binary:./authenticate ./authenticate/authenticate.proto
+protoc --proto_path=./authenticate --js_out=import_style=commonjs,binary:./authenticate ./authenticate/Authenticate.proto
+
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative ./user/User.proto
+
+protoc --proto_path=./user --js_out=import_style=commonjs,binary:./user ./user/User.proto
